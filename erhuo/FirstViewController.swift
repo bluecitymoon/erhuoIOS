d842779c303e58dcda6037084729272871284e12
Jerry Jiang <leeming.jiang@gmail.com>
2014-09-29 14:33:29 +0800

    Initial Commit

---
+++ erhuo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  erhuo
+//
+//  Created by Jerry Jiang on 14/9/29.
+//  Copyright (c) 2014年 Jerry Jiang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ erhuo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  erhuo
+//
+//  Created by Jerry Jiang on 14/9/29.
+//  Copyright (c) 2014年 Jerry Jiang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
